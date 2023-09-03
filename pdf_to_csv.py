6f424461a161c26d6e237f2b31c80e1ee8c2d3e2
sweetcurryice <smartshivam111@gmail.com>
2023-09-04 01:34:30 +0530

    nothing new

--- pdf_to_csv.py
+++
@@ -1,9 +0,0 @@
-import tabula
-
-#lets read the data from pdf into DataFrame
-pdf_file = (r"Victims of Rape under Different Age-Groups During 2015 (StateUT-Wise).pdf")
-
-output_csv = (r"Victims of Rape under Different Age-Groups During 2015.csv")
-
-df = tabula.read_pdf(input_path = pdf_file, pages = 'all')
-tabula.convert_into(input_path = pdf_file, output_path = output_csv, output_format = 'csv', pages = 'all', stream = True)
